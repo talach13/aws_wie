74ebe4521f35b575e9eea55fe984c687ab6b44cb
talach13 <tmc13@mail.aub.edu>
2025-05-27 15:23:52 +0300

    first commit

---
+++ src/model/pokemon.ts
@@ -0,0 +1,15 @@
+// src/model/pokemon.ts
+
+export default interface Pokemon {
+  pokemonNumber: number;
+  attack: number;
+  defense: number;
+  devolution: string;
+  evolution: string;
+  evolutionFamily: string[];
+  healthPoints: number;
+  mainImage: string;
+  pokemonName: string;
+  pokemonType: string[];
+  speed: number;
+}
---
+++ src/model/pokemonCard.ts
@@ -0,0 +1,8 @@
+
+export default interface PokemonCard {
+   pokemonNumber: number;
+   mainImage: string;
+   pokemonName: string;
+   pokemonType: string[];
+}
+
